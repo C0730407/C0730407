532b6f95f4edfa1356dcf111d4c7f8bf35b7fb9c
visualstudio <visualstudio@230-PC06>
2019-01-31 13:17:22 -0500

    first

--- C0730407/Program.cs
+++ C0730407/Program.cs
@@ -10,6 +10,23 @@ namespace C0730407
     {
         static void Main(string[] args)
         {
+            var dogs = new List<string>();
+            dogs.Add("Bichon");
+            dogs.Add("Poodle");
+            dogs.Add("Retriever");
+           foreach (var dog in dogs)
+           {
+                Console.WriteLine(dog + "");
+               
+            }
+            Console.WriteLine(dogs[1]);
+
+            int[] a1 = new int[] { 1, 3, 5, 7, 9, 11 };
+            foreach (int i in a1)
+            {
+                Console.WriteLine(i + "");
+
+            }Console.ReadLine();
         }
     }
 }
